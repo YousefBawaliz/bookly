20a112f51345f616a626568d691ce4231dd3f340
YousefBawaliz <yousef2k012@gmail.com>
2025-11-05 17:15:30 +0300

    added book endpoints

--- main.py
+++ main.py
@@ -1,50 +1,95 @@
-from fastapi import FastAPI, Header
-from typing import Optional
+from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
+books = [
+    {
+        "id": 1,
+        "title": "Think Python",
+        "author": "Allen B. Downey",
+        "publisher": "O'Reilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 1234,
+        "language": "English",
+    },
+    {
+        "id": 2,
+        "title": "Django By Example",
+        "author": "Antonio Mele",
+        "publisher": "Packt Publishing Ltd",
+        "published_date": "2022-01-19",
+        "page_count": 1023,
+        "language": "English",
+    },
+    {
+        "id": 3,
+        "title": "The web socket handbook",
+        "author": "Alex Diaconu",
+        "publisher": "Xinyu Wang",
+        "published_date": "2021-01-01",
+        "page_count": 3677,
+        "language": "English",
+    },
+    {
+        "id": 4,
+        "title": "Head first Javascript",
+        "author": "Hellen Smith",
+        "publisher": "Oreilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 540,
+        "language": "English",
+    },
+    {
+        "id": 5,
+        "title": "Algorithms and Data Structures In Python",
+        "author": "Kent Lee",
+        "publisher": "Springer, Inc",
+        "published_date": "2021-01-01",
+        "page_count": 9282,
+        "language": "English",
+    },
+    {
+        "id": 6,
+        "title": "Head First HTML5 Programming",
+        "author": "Eric T Freeman",
+        "publisher": "O'Reilly Media",
+        "published_date": "2011-21-01",
+        "page_count": 3006,
+        "language": "English",
+    },
+]
+
+class Book(BaseModel):
+    id: int
+    title: str
+    author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
 
 
-@app.get('/greet/{age}')
-async def greet_name(name:str , age:int) -> dict:
-     return {
-            "message" : f"Hello {name}",
-            "age" : f"{age}"
-            }
+@app.get('/books' , response_model=List[Book])
+async def get_all_books() -> list:
+    return books
 
+@app.post('/books')
+async def create_a_book() -> dict:
+    pass 
 
-@app.get('/greet-optional')
-async def greet_name_optional(name: str | None = None) -> dict:
-    if name is None:
-        return {"message": "no name"}
-    else:
-        return {"message": f"{name}"}
+@app.get('/books/{book_id}')
+async def get_a_book(book_id: int) -> dict:
+    for book in books:
+        if book['id'] == book_id:
+            return book        
+    return {"message" : "book not found"}
 
+@app.put('/book/{book_id}')
+async def modify_a_book(book_id: int) :
+    pass
 
-class bookSchema(BaseModel):
-    author:str
-    title:str
-
-@app.post('/create_book')
-async def create_book(book: bookSchema):
-    return {
-        "title" : book.author,
-        "author" : book.author
-    }
-    
-
-@app.get('/get_headers', status_code=201)
-async def get_headers(
-    accept:str =Header(None),
-    content_type: str = Header(None),
-    user_agent: str = Header(None),
-    host:str = Header(None)
-):
-    request_headers = {}
-    request_headers["Accept"] = accept
-    request_headers["content-type"] = content_type
-    request_headers["User-agent"] = user_agent
-    request_headers["host"] = host
-
-    return request_headers
+@app.delete('/books/{book_id}')
+async def delete_a_book(book_id: int) -> dict:
+    pass
